f73f4ac688c4556fa0465e8ff996fb1d9450deb5
SeaPanda <5de6226ufz@gmail.com>
2025-11-05 22:54:37 +0900

    ノードとセーブデータのバージョンを変更した

--- src/main/java/net/seapanda/bunnyhop/common/configuration/BhConstants.java
+++ src/main/java/net/seapanda/bunnyhop/common/configuration/BhConstants.java
@@ -50,7 +50,7 @@ public class BhConstants {
   /** アプリケーションのバージョン. */
   public static final AppVersion APP_VERSION = AppVersion.of("bh-0.7.2");
   /** セーブデータのバージョン. */
-  public static final SaveDataVersion SAVE_DATA_VERSION = SaveDataVersion.of("bhsave-0.7.0");
+  public static final SaveDataVersion SAVE_DATA_VERSION = SaveDataVersion.of("bhsave-0.1.0");
   /** undo 可能な最大回数. */
   public static final int NUM_TIMES_MAX_UNDO = 128;
 
